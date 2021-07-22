8f53caac6e5bdba18a7e061870fbd1eaa56ef54c
ronalddy <ronalddy@gamil.com>
2021-07-22 18:10:26 +0800

    added regularization to logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -12,16 +12,19 @@ def calc_gz(theta, features):
     return np.sum(prod, axis=1)
 
 
-def cost(hyp, labels):
+def cost(hyp, labels, theta, reg_factor=0.0):
     m = hyp.shape[0]
+    reg = reg_factor / 2 / m * np.sum(np.power(theta, 2))
     return -1 / m * np.sum((labels * np.log(hyp)) + ((1-labels) * np.log(1-hyp)))
 
 
-def step(theta, hyp, labels, features, alpha=0.01):
+def step(theta, hyp, labels, features, alpha=0.01, reg_factor=0.0):
     m = hyp.shape[0]
     diff = (hyp - labels).reshape(-1, 1)
     prod = alpha / m * np.sum(diff * features, axis=0)
-    return (theta - prod)
+
+    regd_theta = theta * (1 - (alpha * reg_factor / m))
+    return (regd_theta - prod)
 
 
 def train(
@@ -29,6 +32,7 @@ def train(
         labels, 
         theta_fill=0.5, 
         alpha=0.5,
+        reg_factor=0.0,
         epochs=1000
         ):
     theta = np.full(features.shape[1], theta_fill)
@@ -36,8 +40,16 @@ def train(
     for _ in range(epochs):
         gz = calc_gz(theta, features)
         hyp = hypothesis(gz)
-        theta = step(theta, hyp, labels, features, alpha=alpha)
-        costs.append(cost(hyp, labels))
+        theta = step(
+            theta, 
+            hyp, 
+            labels, 
+            features, 
+            alpha=alpha, 
+            reg_factor=reg_factor)
+        costs.append(cost(hyp, labels, theta, reg_factor))
+
+        print(f'{costs[-1]}\r', end=' ', flush=True)
 
     return theta, costs
     
@@ -52,6 +64,13 @@ def init_data():
 
     split = np.split(data_array, [-1], axis=1)
     features = split[0]
+    features = np.concatenate(
+        (
+            features,
+            np.power(features, 2),
+            np.power(features, 3),
+            ),
+        axis=1)
     labels = split[1].reshape(-1)
 
     f_scale = []
@@ -70,8 +89,10 @@ print(features.shape)
 theta, costs = train(
     features=features,
     labels=labels,
-    alpha=0.5,
-    epochs=2500,
+    theta_fill=0.1,
+    alpha=0.1,
+    reg_factor=10.0,
+    epochs=3000,
     )
 print(theta)
 
